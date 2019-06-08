0b42f66de35af817ac09320b31cdbcedaa80c3a6
LE-xcj <865263907@qq.com>
2019-06-08 23:29:31 +0800

    搭建高可用的Eureka集群，测试客户端向三个注册中心进行服务注册

---
+++ mall-eureka-node/src/main/java/com/xc/mall/MallEurekaNodeApplication.java
@@ -0,0 +1,15 @@
+package com.xc.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MallEurekaNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallEurekaNodeApplication.class, args);
+	}
+
+}
